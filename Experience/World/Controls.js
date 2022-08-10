cdeb5fcfd9de3a1c7fd624ae6351fb1ed8478fb1
Bounar64 <rd.bounar@gmail.com>
2022-08-10 19:43:56 +0200

    ScrollTrigger Room animation + responsive

--- Experience/World/Controls.js
+++ Experience/World/Controls.js
@@ -1,14 +1,40 @@
-import * as THREE from 'three';
-import Experience from '../Experience.js';
-import GSAP from 'gsap';
+import * as THREE from "three";
+import Experience from "../Experience.js";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger.js";
 
 export default class Controls {
     constructor() {
         this.experience = new Experience();
         this.scene = this.experience.scene;
+        this.sizes = this.experience.sizes;
         this.resources = this.experience.resources;
         this.time = this.resources.time;
         this.camera = this.experience.camera;
+        this.room = this.experience.world.room.actualRoom;
+        gsap.registerPlugin(ScrollTrigger);
+
+        this.setPath();
+    }
+
+    setPath() {
+        this.timeline = new gsap.timeline();
+        this.timeline.to(this.room.position, {
+            x: () => {
+                return this.sizes.width * 0.0009;
+            },
+            z: () => {
+                return -(this.sizes.width * 0.0009);
+            },
+            scrollTrigger: {
+                trigger: '.first-move',
+                markers: true,
+                start: 'top top',
+                end: 'bottom bottom',
+                scrub: 0.5,
+                invalidateOnRefresh: true,
+            }
+        });
     }
 
     resize() {}
--- Experience/World/Room.js
+++ Experience/World/Room.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import Experience from '../Experience.js';
-import GSAP from 'gsap';
+import gsap from 'gsap';
 
 export default class Room {
     constructor() {
@@ -61,7 +61,7 @@ export default class Room {
 
     update() {
 
-        this.lerp.current = GSAP.utils.interpolate(
+        this.lerp.current = gsap.utils.interpolate(
             this.lerp.current,
             this.lerp.target,
             this.lerp.ease
